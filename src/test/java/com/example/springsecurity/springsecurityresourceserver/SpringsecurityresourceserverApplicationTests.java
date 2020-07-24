806cfb7f9f59cc24521b15d035fba9a38065416d
Dilun-Panduka <dilunp4@gmail.com>
2020-07-24 12:14:43 +0530

    initial commit

---
+++ src/test/java/com/example/springsecurity/springsecurityresourceserver/SpringsecurityresourceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.springsecurityresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityresourceserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
